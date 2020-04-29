433d082fa7c0912286c6b6b9d806ed2fb4f5b306
Jhon Chavez <jhchg7@gmail.com>
2020-04-28 21:26:30 -0400

    Bubble sort: main code

--- bubbleSort.c
+++ bubbleSort.c
@@ -10,7 +10,7 @@ void cambiar_pos(int *n1, int *n2)
     *n2 = temp;
 }
 
-void bubble_sort(int vector_entrada[], n)
+void bubbleSort(int vector_entrada[], int n)
 {
     int i, j;
     for (i=0; i < n-1; i++)
@@ -22,4 +22,23 @@ void bubble_sort(int vector_entrada[], n)
         }
     }
     
+}
+
+int print_array(int vector_entrada[], int n)
+{
+    int i;
+    for(i=0; i<n-1; i++)
+        printf("%d  ", vector_entrada[i]);
+    printf("\n fin del Ordenamiento");
+}
+
+main(int argc, char const *argv[])
+{
+    int vector_entrada[]={100, 1992, 0, -1, 60, 70, 14, 15, 10};
+    int n = sizeof(vector_entrada)/sizeof(vector_entrada[0]);
+    // int n = sizeof(vector_entrada)/sizeof(int);
+    bubbleSort(vector_entrada, n);
+    print_array(vector_entrada, n);
+    printf("\n");
+    return 0;
 }
